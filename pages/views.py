f5afa17fe0f45490e812c6ffabcac68ee6968d91
boracakirel <boracakirel@gmail.com>
2022-08-10 18:39:40 +0300

    added about and contact view

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import ContactForm
+from django.urls import reverse_lazy
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactView(FormView, SuccessMessageMixin):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+    success_message = 'Your message has been sent successfully'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
