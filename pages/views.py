1cb55880fa4d0592369644663b60876d572facd7
boracakirel <boracakirel@gmail.com>
2022-09-28 16:37:47 +0300

    added coingecko API

--- pages/views.py
+++ pages/views.py
@@ -4,17 +4,27 @@ from django.views.generic.edit import FormView
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import ContactForm
 from django.urls import reverse_lazy
+from pycoingecko import CoinGeckoAPI
+import locale
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+def index(request):
+    cg = CoinGeckoAPI()
+    result = cg.get_coins_markets('usd')
+    #
+    # coins = {}
+    # for coin in result:
+    #     coins.add
+    # locale.setlocale(locale.LC_ALL, '')
+    # result = [{'name': 'bitcoin', 'image': 'btc.png', 'current_price': locale.currency(18921.54), 'market_cap': locale.currency(132134)}]
+    return render(request, 'index.html', context={'coins': result})
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
 
 
-class ContactView(FormView, SuccessMessageMixin):
+class ContactView(SuccessMessageMixin, FormView):
     template_name = 'contact.html'
     form_class = ContactForm
     success_url = reverse_lazy('contact')
