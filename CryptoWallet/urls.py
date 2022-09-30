a301ba5fc26ef307ebfa83c7a50ba83554807631
boracakirel <boracakirel@gmail.com>
2022-09-30 23:33:45 +0300

    added accounts url path

--- CryptoWallet/urls.py
+++ CryptoWallet/urls.py
@@ -20,5 +20,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
+
+
+
 
 ]
