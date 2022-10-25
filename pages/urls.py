5a27d26cb9e974bd7ecccf711a724678e962ca96
boracakirel <boracakirel@gmail.com>
2022-10-25 23:27:05 +0300

    added coin detail url path

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('currencies/', views.currency, name='currencies'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('coins/<str:coin_id>', views.coin_detail, name='detail'),
 ]
