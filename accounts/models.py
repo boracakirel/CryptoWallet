5a1c3599e8307e6eabdcab49ba6027084f80084b
boracakirel <boracakirel@gmail.com>
2022-11-01 02:02:07 +0300

    added Transaction model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Transaction(models.Model):
+    price = models.DecimalField(decimal_places=10, max_digits=100)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now=True)
+    amount = models.DecimalField(decimal_places=10, max_digits=100)
+    user = models.ManyToManyField(User, blank=True, related_name='transactions')
+    coin_id = models.CharField(max_length=100)
+
