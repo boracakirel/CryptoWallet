8fc8d3f9ac8a0bed96cff70fac35f9d15f94e754
boracakirel <boracakirel@gmail.com>
2022-11-06 19:31:35 +0300

    added transactions url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('logout/', views.user_logout, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('add/', views.add_crypto, name='add_crypto'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('transactions/<str:coin_id>', views.transactions, name='transactions'),
 ]
