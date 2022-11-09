2af779548e3c8c736bc55ccfa8d12b6a012aeec9
boracakirel <boracakirel@gmail.com>
2022-11-09 17:50:16 +0300

    added edit and delete url paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add/', views.add_crypto, name='add_crypto'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('transactions/<str:coin_id>', views.transactions, name='transactions'),
+    path('edit_transaction/', views.edit_transaction, name='edit'),
+    path('transactions/<str:coin_id>/delete/<int:transaction_id>', views.delete_transaction, name='delete'),
 ]
